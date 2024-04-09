b4cf5507d4a3e2e73c705693d0ba0101553318a0
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-04-09 18:53:05 +0400

    LogTime

--- web-app/backend/database/migrations/2024_04_06_162430_change_column_to_attendance_logs.php
+++ web-app/backend/database/migrations/2024_04_06_162430_change_column_to_attendance_logs.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,8 +15,14 @@ return new class extends Migration
     public function up()
     {
         Schema::table('attendance_logs', function (Blueprint $table) {
-            $table->dateTime("LogTime")->nullable()->change();
+            // $table->dateTime("LogTime")->nullable()->change();
+
+
+            $table->timestamp('LogTime')->nullable()->change();
         });
+
+        // Specify the conversion
+        DB::statement('ALTER TABLE attendance_logs ALTER COLUMN "LogTime" TYPE timestamp without time zone USING "LogTime"::timestamp without time zone');
     }
 
     /**
@@ -26,7 +33,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('attendance_logs', function (Blueprint $table) {
-            //
+            $table->timestamp('LogTime')->nullable()->change();
         });
     }
 };
